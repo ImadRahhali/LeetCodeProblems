11e69b20b1137bff0a0c35bb45080e0c4e9dbbd0
Imad Rahhali <imadrahhali5@gmail.com>
2025-03-31 17:16:22 +0000

    N/A (0%) N/A (0%) 03-31-2025 0169-majority-element Easy .py

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter={}
-        for i in range(len(nums)):
-            if nums[i] in counter:
-                counter[nums[i]] += 1
-            else:
-                counter[nums[i]] = 1
-        for num in counter:
-            if counter[num] > len(nums)/2:
-                return num
+        res , count = 0,0
+        for num in nums:
+            if count == 0:
+                res = num
+                count += 1
+            count += (1 if num == res else -1)
+            
+            print(res)
+            print(count)
+        return res
