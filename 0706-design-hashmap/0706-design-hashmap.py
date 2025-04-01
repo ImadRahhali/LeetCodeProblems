0cad31b8732157463c14fc47c2ed31ade6750900
Imad Rahhali <imadrahhali5@gmail.com>
2025-04-01 16:30:32 +0000

    131 ms (18.07%) 22.9 MB (24.29%) 04-01-2025 0706-design-hashmap Easy .py

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,31 +1,32 @@
 class ListNode:
-    def __init__(self, pair):
-        self.pair = pair
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
         self.next = None
 class MyHashMap:
     def __init__(self):
-        self.set = [ListNode([0,0]) for i in range(10**4)]
+        self.set = [ListNode(0,0) for i in range(10**4)]
     def put(self, key: int, value: int) -> None:
         curr = self.set[key % 10**4]
         while curr.next:
-            if curr.next.pair[0] == key:
-                curr.next.pair[1] = value
+            if curr.next.key == key:
+                curr.next.val = value
                 return
             curr = curr.next
-        curr.next = ListNode([key,value])
+        curr.next = ListNode(key,value)
 
     def get(self, key: int) -> int:
         curr = self.set[key % 10**4]
         while curr.next:
-            if curr.next.pair[0] == key:
-                return curr.next.pair[1]                
+            if curr.next.key == key:
+                return curr.next.val               
             curr = curr.next
         return -1
 
     def remove(self, key: int) -> None:
         curr = self.set[key % 10**4]
         while curr.next:
-            if curr.next.pair[0] == key:
+            if curr.next.key == key:
                 curr.next = curr.next.next
                 return
             curr = curr.next
