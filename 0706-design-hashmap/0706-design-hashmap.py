a0ff9e9b208420cdbfbd3e82b63cb1c4922ed46e
Imad Rahhali <imadrahhali5@gmail.com>
2025-04-01 16:21:44 +0000

    135 ms (17.87%) 24 MB (20.53%) 04-01-2025 0706-design-hashmap Easy .py

---
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,38 @@
+class ListNode:
+    def __init__(self, pair):
+        self.pair = pair
+        self.next = None
+class MyHashMap:
+    def __init__(self):
+        self.set = [ListNode([0,0]) for i in range(10**4)]
+    def put(self, key: int, value: int) -> None:
+        curr = self.set[key % 10**4]
+        while curr.next:
+            if curr.next.pair[0] == key:
+                curr.next.pair[1] = value
+                return
+            curr = curr.next
+        curr.next = ListNode([key,value])
+
+    def get(self, key: int) -> int:
+        curr = self.set[key % 10**4]
+        while curr.next:
+            if curr.next.pair[0] == key:
+                return curr.next.pair[1]                
+            curr = curr.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        curr = self.set[key % 10**4]
+        while curr.next:
+            if curr.next.pair[0] == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next   
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
