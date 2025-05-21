7707e7e352c943341161bb22997d7f21ce3772b8
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-21 17:29:16 +0100

    3 ms (50.74%) 19.1 MB (24.35%) 05-21-2025 0001-two-sum Easy .py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         sumDic = {}
         for i in range(len(nums)):
-            sumDic[nums[i]] = i
-        for i in range(len(nums)):
             diff = target - nums[i]
-            if diff in sumDic and i != sumDic[diff]:
-                return [i, sumDic[diff]] 
+            if diff in sumDic:
+                return [i, sumDic[diff]]
+            sumDic[nums[i]] = i
+             
