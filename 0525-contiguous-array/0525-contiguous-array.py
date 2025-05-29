0eb4f13c3450e1b1c90eb74fee4513cdd49657a9
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-30 00:29:18 +0100

    110 ms (32.55%) 23.5 MB (22.46%) 05-30-2025 0525-contiguous-array Medium .py

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,13 +1,11 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums[i] = -1
-        
         currSum = 0
         pre = {0:0}
         max_length = 0
         for i in range (len(nums)):
+            if nums[i] == 0:
+                nums[i] = -1
             length = 0
             currSum += nums[i]
             if currSum in pre:
@@ -18,7 +16,6 @@ class Solution:
             
         return max_length
 
-#{0:0, -1:1,}
 
             
         
