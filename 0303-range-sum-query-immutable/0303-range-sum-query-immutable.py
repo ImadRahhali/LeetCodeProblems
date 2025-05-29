0f19306d2f09b8cd91105f0410ce0d36178c003d
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-29 11:42:01 +0100

    9 ms (33.8%) 21.7 MB (76.53%) 05-29-2025 0303-range-sum-query-immutable Easy .py

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -0,0 +1,18 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        total = 0
+        self.prefix_sums = []
+        for n in nums:
+            total += n
+            self.prefix_sums.append(total)
+
+    def sumRange(self, left: int, right: int) -> int:
+        left_sum = self.prefix_sums[left-1] if left > 0 else 0
+        right_sum = self.prefix_sums[right]
+        return right_sum - left_sum
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
