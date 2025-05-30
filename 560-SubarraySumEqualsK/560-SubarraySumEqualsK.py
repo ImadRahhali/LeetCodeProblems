1800243388add13fbe244c3579e3c665f4f2922d
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-30 19:34:33 +0100

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -0,0 +1,12 @@
+# Last updated: 5/30/2025, 7:34:33 PM
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        pre = { 0 : 1}
+        currSum = 0
+        count = 0
+        for num in nums:
+            currSum += num
+            diff = currSum - k
+            count += pre.get(diff,0)
+            pre[currSum] = 1 + pre.get(currSum,0)
+        return count
