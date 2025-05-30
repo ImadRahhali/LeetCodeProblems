bacf3572a0dae360ae3f9dce25a4b3cd398c2736
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-30 19:34:59 +0100

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,26 @@
+// Last updated: 5/30/2025, 7:34:59 PM
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !alphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !alphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++; r--;
+        }
+        return true;
+    }
+
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' || 
+                c >= 'a' && c <= 'z' || 
+                c >= '0' && c <= '9');
+    }
+}
