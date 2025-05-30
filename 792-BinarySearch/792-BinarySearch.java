28c1f82fdaddd75f69609c0f551c6ebbfe57f9c3
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-30 19:34:26 +0100

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,22 @@
+// Last updated: 5/30/2025, 7:34:26 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int L = 0;
+        int R = nums.length-1;
+        while (L<=R) {
+            int mid = (L+R)/2;
+            if (target < nums[mid]) {
+                R = mid -1;
+            }
+            else if (nums[mid]<target) {
+                L= mid+1;
+            }
+            else {
+                return mid;
+            }
+            
+        }
+        return -1;
+    }    
+
+}
