df6cd86540fef0a0ba833c27c6780a4909bd622a
Imad Rahhali <imadrahhali5@gmail.com>
2025-05-31 14:00:57 +0100

    File updated at 5/31/2025, 2:00:57 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,13 +1,14 @@
-# Last updated: 5/31/2025, 1:59:04 PM
+# Last updated: 5/31/2025, 2:00:57 PM
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         max_length = 0
         L = 0
         mp = {}
+        max_freq = 0
         for R in range(len(s)):
             mp[s[R]] = 1 + mp.get(s[R], 0)
-            diff_chars = (R-L+1) - max(mp.values())
-            if diff_chars > k:
+            max_freq = max(max_freq, mp[s[R]])
+            if (R - L + 1) - max_freq > k:
                 mp[s[L]] -= 1
                 L += 1
             max_length = max(max_length, R - L + 1)
