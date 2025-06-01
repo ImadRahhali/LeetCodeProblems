a16d4cdeaa8aa9bb9ee7629508929b9708ce2984
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-01 12:39:34 +0100

    File updated at 6/1/2025, 12:39:34 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -1,12 +1,16 @@
-# Last updated: 5/31/2025, 10:16:54 PM
+# Last updated: 6/1/2025, 12:39:34 PM
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        L, R = 0, len(nums) -1
-        while L < R:
-            mid = (L+R)//2
+        L, R = 0, len(nums) - 1
+        res = nums[0]
+
+        while L <= R:
+            mid = (L + R) // 2
+            res = min(res, nums[mid])
+
             if nums[mid] > nums[R]:
                 L = mid + 1
             else:
-                R = mid
-        return nums[L]
+                R = mid - 1
 
+        return res
