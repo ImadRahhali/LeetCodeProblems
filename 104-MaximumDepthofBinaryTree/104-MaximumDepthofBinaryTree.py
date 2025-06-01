3487b0c8923abfbc354e400ef359e5ded056e260
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-01 23:02:42 +0100

    Create 104-MaximumDepthofBinaryTree

---
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -0,0 +1,24 @@
+# Last updated: 6/1/2025, 11:02:42 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        queue = deque()
+
+        if root:
+            queue.append(root)
+        max_depth = 0
+        while queue:
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            max_depth+=1
+        return max_depth
