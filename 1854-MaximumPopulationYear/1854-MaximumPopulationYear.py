dce43ee2b316df2c460e1506621b755188c721b4
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-02 21:30:29 +0100

    File updated at 6/2/2025, 9:30:29 PM

--- 1854-MaximumPopulationYear/1854-MaximumPopulationYear.py
+++ 1854-MaximumPopulationYear/1854-MaximumPopulationYear.py
@@ -1,17 +1,21 @@
-# Last updated: 6/2/2025, 9:13:34 PM
+# Last updated: 6/2/2025, 9:30:29 PM
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        max_year = 1950
+        pop_changes = [0] * 101
+        for birth, death in logs:
+            pop_changes[birth - 1950]+=1
+            pop_changes[death - 1950]-=1
+        
+        curr_pop = 0
         max_pop = 0
+        max_year = 0
 
-        for year in range(1950, 2051):
-            population = 0
-            for birth, death in logs:
-                if birth <= year < death:
-                    population += 1
-                
-            if population > max_pop:
-                max_pop = population
-                max_year = year
-
+        for i in range(101):
+            curr_pop += pop_changes[i]
+            if curr_pop > max_pop:
+                max_pop = curr_pop
+                max_year = 1950 + i
         return max_year
+
+
+        
