17ae87de8f5fac5dbf6c2bf32e907f1cc2163c09
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-05 21:17:29 +0100

    File updated at 6/5/2025, 9:17:28 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,14 +1,13 @@
-# Last updated: 6/5/2025, 9:11:32 PM
+# Last updated: 6/5/2025, 9:17:28 PM
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
         count = 0
         def dfs(r, c):
             if min(r, c) < 0 or r >= len(grid) or c >= len(grid[0])\
-            or grid[r][c] == '0' or (r,c) in visited:
+            or grid[r][c] == '0':
                 return
 
-            visited.add((r,c))
+            grid[r][c] = '0'
             dfs(r+1, c)
             dfs(r-1, c)
             dfs(r, c+1)
@@ -16,7 +15,7 @@ class Solution:
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1' and (i, j) not in visited:
+                if grid[i][j] == '1':
                     count += 1
                     dfs(i, j)
         return count
