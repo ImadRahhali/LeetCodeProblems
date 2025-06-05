8b6a9ab5b0b975bf9a09b14e775a7287e63ae9c1
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-05 19:56:01 +0100

    File updated at 6/5/2025, 7:56:00 PM

--- 463-IslandPerimeter/463-IslandPerimeter.py
+++ 463-IslandPerimeter/463-IslandPerimeter.py
@@ -1,18 +1,19 @@
-# Last updated: 6/5/2025, 7:45:08 PM
+# Last updated: 6/5/2025, 7:56:00 PM
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         count = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == 0:
-                    continue
-                else:
-                    if r == 0 or grid[r-1][c] == 0:
-                        count += 1
-                    if r == len(grid) - 1 or grid[r+1][c] == 0:
-                        count += 1
-                    if c == 0 or grid[r][c-1] == 0:
-                        count += 1
-                    if c == len(grid[0]) - 1 or grid[r][c+1] == 0:
-                        count += 1
+                edges = 0
+                if grid[r][c] == 1:
+                    edges = 4
+                    if r > 0 and grid[r-1][c] == 1:
+                        edges -= 1
+                    if r < len(grid) - 1 and grid[r+1][c] == 1:
+                        edges -= 1
+                    if c > 0 and grid[r][c-1] == 1:
+                        edges -= 1
+                    if c < len(grid[0]) - 1 and grid[r][c+1] == 1:
+                        edges -= 1
+                count += edges
         return count
