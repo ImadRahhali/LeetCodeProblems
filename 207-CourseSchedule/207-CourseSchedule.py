8dc86f8abe9b0d76b224ea41d9d6950d43632e2b
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-07 14:40:03 +0100

    File updated at 6/7/2025, 2:40:03 PM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,30 +1,28 @@
-# Last updated: 6/7/2025, 2:10:14 PM
+# Last updated: 6/7/2025, 2:40:03 PM
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pre = {i: [] for i in range(numCourses)}
+        preMap = {i: [] for i in range(numCourses)}
         for a, b in prerequisites:
-            pre[a].append(b)
-        
-        visited, path = set(), set()
-        def dfs(node):
-            if node in path:
+            preMap[a].append(b)
+       
+        visited = set()
+
+        def dfs(course):
+            if course in visited:
                 return False
-            if node in visited:
+            if preMap[course] == []:
                 return True
+            
+            visited.add(course)
 
-            path.add(node)
-
-            for prereq in pre[node]:
-                if not dfs(prereq):
+            for pre in preMap[course]:
+                if not dfs(pre):
                     return False
-            path.remove(node)
-            visited.add(node)
+            visited.remove(course)
+            preMap[course] = []
             return True
 
         for course in range(numCourses):
             if not dfs(course):
                 return False
         return True
-
-
-
