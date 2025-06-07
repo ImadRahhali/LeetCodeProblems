c9ff25f0b3d3160c2e27aac2989a649dd75b1923
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-07 15:35:58 +0100

    File updated at 6/7/2025, 3:35:57 PM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,4 +1,4 @@
-# Last updated: 6/7/2025, 3:35:21 PM
+# Last updated: 6/7/2025, 3:35:57 PM
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         preMap = {i: [] for i in range(numCourses)}
@@ -6,21 +6,20 @@ class Solution:
             preMap[a].append(b)
        
         visited = set()
-        cycle = set()
 
         def dfs(course):
-            if course in cycle:
-                return False
             if course in visited:
+                return False
+            if preMap[course] == []:
                 return True
             
-            cycle.add(course)
+            visited.add(course)
 
             for pre in preMap[course]:
                 if not dfs(pre):
                     return False
-            cycle.remove(course)
-            visited.add(course)
+            visited.remove(course)
+            preMap[course] = []
             return True
 
         for course in range(numCourses):
