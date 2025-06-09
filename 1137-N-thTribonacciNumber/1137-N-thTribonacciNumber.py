8f3aa76bd76100514a1e7feed1a865d0055286db
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-09 20:33:35 +0100

    File updated at 6/9/2025, 8:33:35 PM

--- 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.py
+++ 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.py
@@ -1,17 +1,18 @@
-# Last updated: 6/9/2025, 8:29:51 PM
+# Last updated: 6/9/2025, 8:33:35 PM
 class Solution:
     def tribonacci(self, n: int) -> int:
-        cache = {}
-        def memo(n):
-            if n <= 1:
-                return n
-            if n == 2:
-                return 1
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
 
-            if n in cache:
-                return cache[n]
+        dp = [0,1,1]
+        i = 3
+        while i <= n:
+            tmp = dp[2]
+            dp[2] = dp[0] + dp[1] + dp[2]
+            dp[0] = dp[1]
+            dp[1] = tmp
+            i += 1
+        return dp[2]
         
-            cache[n] = memo(n-1) + memo(n-2) + memo(n-3)
-            return cache[n]
-        
-        return memo(n)
