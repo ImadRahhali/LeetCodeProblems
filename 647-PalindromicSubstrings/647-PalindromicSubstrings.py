1a456b70c408a95c4ccedd5a65c8f044093a4544
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-10 14:16:29 +0100

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -0,0 +1,16 @@
+# Last updated: 6/10/2025, 2:16:28 PM
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        return count
