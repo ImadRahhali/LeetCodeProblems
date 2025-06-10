6e1aa7c3c0bf6c483e884689be9389aa17c3ac75
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-10 14:18:19 +0100

    File updated at 6/10/2025, 2:18:19 PM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.py
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -1,16 +1,14 @@
-# Last updated: 6/10/2025, 2:16:28 PM
+# Last updated: 6/10/2025, 2:18:19 PM
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
+        def helper(l, r):
+            nonlocal count
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
         for i in range(len(s)):
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                count += 1
-                l -= 1
-                r += 1
-            l, r = i, i+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                count += 1
-                l -= 1
-                r += 1
+            helper(i, i)
+            helper(i, i+1)
         return count
