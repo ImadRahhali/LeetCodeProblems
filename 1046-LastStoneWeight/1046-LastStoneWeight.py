8e90acd809a08647c89cda1546582428affaee99
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-11 13:58:48 +0100

    File updated at 6/11/2025, 1:58:48 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.py
+++ 1046-LastStoneWeight/1046-LastStoneWeight.py
@@ -1,13 +1,13 @@
-# Last updated: 6/11/2025, 1:57:26 PM
+# Last updated: 6/11/2025, 1:58:48 PM
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         maxheap = [-x for x in stones]
         heapq.heapify(maxheap)
         while len(maxheap) > 1:
-            first = - heapq.heappop(maxheap)
-            second = - heapq.heappop(maxheap)
+            first = heapq.heappop(maxheap)
+            second = heapq.heappop(maxheap)
             if first != second:
-                heapq.heappush(maxheap, - (first - second))
+                heapq.heappush(maxheap, first - second)
         return - maxheap[0] if len(maxheap) == 1 else 0
 
 
