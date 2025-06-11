c426f2cce3ba3be4629ccf27241c7dbba033c7f6
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-11 19:20:13 +0100

    File updated at 6/11/2025, 7:20:13 PM

--- 682-BaseballGame/682-BaseballGame.py
+++ 682-BaseballGame/682-BaseballGame.py
@@ -1,4 +1,4 @@
-# Last updated: 6/11/2025, 7:19:34 PM
+# Last updated: 6/11/2025, 7:20:13 PM
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
@@ -6,9 +6,9 @@ class Solution:
             if i == 'C':
                 stack.pop()
             elif i == 'D':
-                stack.append(int(stack[-1]*2))
+                stack.append(stack[-1] * 2)
             elif i == '+':
-                stack.append(int(stack[-1]+stack[-2]))
+                stack.append(stack[-1] + stack[-2])
             else:
                 stack.append(int(i))
         return sum(stack)
