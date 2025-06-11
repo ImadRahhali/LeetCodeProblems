21255ad48f2e742983eb4a9781c662c6623947ef
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-11 19:03:22 +0100

    File updated at 6/11/2025, 7:03:21 PM

--- 155-MinStack/155-MinStack.py
+++ 155-MinStack/155-MinStack.py
@@ -1,20 +1,28 @@
-# Last updated: 6/11/2025, 6:50:58 PM
+# Last updated: 6/11/2025, 7:03:21 PM
+class Node:
+    def __init__(self, currentMin, val):
+        self.val = val
+        self.min = currentMin
 class MinStack:
 
     def __init__(self):
         self.arr = []
 
     def push(self, val: int) -> None:
-        self.arr.append(val)
+        if not self.arr:
+            currentMin = val
+        else:
+            currentMin = min(val, self.arr[-1].min)
+        self.arr.append(Node(currentMin, val))
 
     def pop(self) -> None:
         self.arr.pop()
 
     def top(self) -> int:
-        return self.arr[-1]
+        return self.arr[-1].val
 
     def getMin(self) -> int:
-        return min(self.arr)
+        return self.arr[-1].min
 
 
 # Your MinStack object will be instantiated and called as such:
