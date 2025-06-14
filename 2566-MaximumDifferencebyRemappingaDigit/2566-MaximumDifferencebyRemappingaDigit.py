2fa3c5fa990ab21915b27ccfedc7d0e25167564f
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-14 11:44:01 +0100

    Create 2566-MaximumDifferencebyRemappingaDigit

---
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -0,0 +1,10 @@
+# Last updated: 6/14/2025, 11:44:01 AM
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        num = str(num)
+        i = 0
+
+        while num[i] == '9' and i < len(num) - 1:
+            i+= 1
+        
+        return int(num.replace(num[i], "9")) - int(num.replace(num[0], "0"))
