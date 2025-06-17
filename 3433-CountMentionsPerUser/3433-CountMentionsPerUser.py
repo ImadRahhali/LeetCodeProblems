b3fc2f06ea280945bf07388b7715a92596d53482
Imad Rahhali <imadrahhali5@gmail.com>
2025-06-17 20:29:08 +0100

    Create 3433-CountMentionsPerUser

---
+++ 3433-CountMentionsPerUser/3433-CountMentionsPerUser.py
@@ -0,0 +1,21 @@
+# Last updated: 6/17/2025, 8:29:08 PM
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        max_freq = 0
+        initial_count = nums.count(k)
+        for target in set(nums):
+            if target == k:
+                continue
+            
+            curr_gain, max_gain = 0, 0
+            for num in nums:
+                if num == target:
+                    curr_gain += 1
+                elif num == k:
+                    curr_gain -= 1
+                
+                max_gain = max(max_gain, curr_gain)
+                curr_gain = max(curr_gain, 0)
+
+            max_freq = max(max_freq, max_gain)
+        return initial_count + max_freq
