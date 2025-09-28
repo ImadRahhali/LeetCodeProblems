95b57714d97f7d411fd1acd0b21301cfdc961bdf
Imad Rahhali <imadrahhali5@gmail.com>
2025-09-28 20:38:46 +0100

    Add .py solution

---
+++ unknown_problem_1759088325744/.py
@@ -0,0 +1,12 @@
+class Solution:class Solution:
+    def largestPerimeter(self, nums: List    def largestPerimeter(self, nums: List
+[int]) -> int:[int]) -> int:
+        nums.sort(reverse=True)        nums.sort(reverse=True)
+        for i in range(len(nums) - 2):        for i in range(len(nums) - 2):
+            if nums[i+1] + nums[i+2] > nums[i]:            if nums[i+1] + nums[i+2] > nums[i]:
+                return nums[i+1] + nums[i+2] +                 return nums[i+1] + nums[i+2] + 
+nums[i]nums[i]
+        return 0        return 0
+
+
+                        
