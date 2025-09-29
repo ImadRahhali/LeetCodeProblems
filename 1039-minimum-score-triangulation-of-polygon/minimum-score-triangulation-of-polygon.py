99d4f53486891794d6605beee0aa6bf913eeb2c5
Imad Rahhali <imadrahhali5@gmail.com>
2025-09-29 20:26:06 +0100

    Add minimum-score-triangulation-of-polygon.py solution

--- 1039-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
+++ 1039-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
@@ -1,13 +1,13 @@
 class Solution:
-    def minScoreTriangulation(self, 
-values: List[int]) -> int:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        @lru_cache(None)
         def dp(i, j):
             if j - i < 2: return 0
 
             res = float('inf')
 
             for k in range(i+1, j):
-                curr = values[i] * values
-[j] * values[k]
-                res = min(res, curr + dp
-        @cache
+                curr = values[i] * values[j] * values[k]
+                res = min(res, curr + dp(i, k) + dp(k, j))
+            
+            return res
