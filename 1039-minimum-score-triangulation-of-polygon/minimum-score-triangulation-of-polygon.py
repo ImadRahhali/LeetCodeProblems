b1e8dfebc01282f16e59da457e2171f2754e82ed
Imad Rahhali <imadrahhali5@gmail.com>
2025-09-29 20:30:33 +0100

    Update minimum-score-triangulation-of-polygon.py

--- 1039-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
+++ 1039-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
@@ -1,13 +1,15 @@
-            if j - i < 2: return 0
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        @cache
+        def dp(i, j):
+            if j - i < 2: return 0
 
-            res = float('inf')
+            res = float('inf')
 
-            for k in range(i+1, j):
-        def dp(i, j):
-                curr = values[i] * values[j] * values[k]
-                res = min(res, curr + dp(i, k) + dp(k, j))
-            
-·‌·‌·‌·‌·‌·‌·‌·‌@cache
-    def minScoreTriangulation(self, values: List[int]) -> int:
-class Solution:
-            return res
+            for k in range(i+1, j):
+                curr = values[i] * values[j] * values[k]
+                res = min(res, curr + dp(i, k) + dp(k, j))
+            
+            return res
+        
+        return dp(0, len(values) - 1)
