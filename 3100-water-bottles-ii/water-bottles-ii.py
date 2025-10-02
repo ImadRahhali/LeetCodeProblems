894c7a1e3001db6081780ad2f235d24bab5fe1e0
Imad Rahhali <imadrahhali5@gmail.com>
2025-10-02 11:06:27 +0100

    Add water-bottles-ii.py solution

--- 3100-water-bottles-ii/water-bottles-ii.py
+++ 3100-water-bottles-ii/water-bottles-ii.py
@@ -1,9 +1,10 @@
-                numExchange += 1
-            #Drink
-            else:
-                counter += full
-                empty += full
-                full = 0
-            numBottles = full + empty
-        counter += full
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        counter = numBottles
+        empty = numBottles
+
+        while empty >= numExchange:
+            counter += 1
+            empty -= numExchange - 1
+            numExchange += 1
         return counter
